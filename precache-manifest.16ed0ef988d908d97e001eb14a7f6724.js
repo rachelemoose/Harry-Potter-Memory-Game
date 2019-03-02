61c3010465e17c9ddd0592cce165d68f45ea9da4
R.E. Moose <rachelemoose@gmail.com>
2019-03-02 15:19:43 -0500

    Updates

---
+++ precache-manifest.16ed0ef988d908d97e001eb14a7f6724.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f0878d139fe2eda74f1",
+    "url": "/Harry-Potter-Memory-Game/static/css/main.7c6b30ec.chunk.css"
+  },
+  {
+    "revision": "3f0878d139fe2eda74f1",
+    "url": "/Harry-Potter-Memory-Game/static/js/main.3f0878d1.chunk.js"
+  },
+  {
+    "revision": "f961a142c74d3ad95944",
+    "url": "/Harry-Potter-Memory-Game/static/js/runtime~main.f961a142.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/Harry-Potter-Memory-Game/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "9173793cf3427e68a8253c484e5f52e6",
+    "url": "/Harry-Potter-Memory-Game/index.html"
+  }
+];
