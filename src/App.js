7d5730d3220d72c992326f9db3f8eddfeac9acf1
R.E. Moose <rachelemoose@gmail.com>
2019-03-04 15:56:22 -0500

    final functionality

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Jumbotron from "./components/Jumbotron";
 import WizardCard from "./components/WizardCard";
 import wizard from "./wizard.json";
 
-//sets state to 0 or empty
+// sets state to 0 or empty for array of Wizards that have been clicked
 class App extends Component {
   state = {
     wizard,
@@ -13,6 +13,52 @@ class App extends Component {
     score: 0
   };
 
+// when you click on an image, capture image clicked as a wizard clicked
+imageClick = event => {
+  const currentWizard = event.target.alt;
+  const wizardClicked =
+    this.state.clickedWizard.indexOf(currentWizard) > -1;
+
+// if you click on a wizard that has already been selected, the game is reset and cards reordered
+  if (wizardClicked) {
+    this.setState({
+      wizard: this.state.wizard.sort(function(a, b) {
+        return 0.5 - Math.random();
+      }),
+      clickedWizard: [],
+      score: 0
+    });
+      alert("You already clicked that Wizard! You've lost. Play again?");
+
+// if you click on a new wizard, your score is increased and cards reordered
+  } else {
+    this.setState(
+      {
+        wizard: this.state.wizard.sort(function(a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedWizard: this.state.clickedWizard.concat(
+          currentWizard
+        ),
+        score: this.state.score + 1
+      },
+
+//if you get all 12 corrent you get a congrats message and the game resets        
+      () => {
+        if (this.state.score === 12) {
+          alert("Congrats! You Win!");
+          this.setState({
+            wizard: this.state.wizard.sort(function(a, b) {
+              return 0.5 - Math.random();
+            }),
+            clickedWizard: [],
+            score: 0
+          });
+        }
+      }
+    );
+  }
+};
 
   render() {
     return (
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,11 +6,9 @@ class Navbar extends Component {
     render() {
         return (
         <nav className="navbar navbar-default navbar-fixed-top">
-        <ul>
             <li className="clickyLeft">Clicky Game</li>
             <li className="clickyMiddle">Click an image to begin!</li>
             <li className="clickyScoreRight">Score: {this.props.score}</li>
-        </ul>
         </nav>
         );
     }
